c4d30b9f2460697699eab6bec4731d1af482066b
SofiaNords <sofia.k.nordstrom.com>
2024-11-17 08:04:18 +0100

    Add shuffledAnswers to PlayerView

--- Labb3_Quiz_Configurator/ViewModel/PlayerViewModel.cs
+++ Labb3_Quiz_Configurator/ViewModel/PlayerViewModel.cs
@@ -9,11 +9,13 @@ namespace Labb3_Quiz_Configurator.ViewModel
     {
         // En privat variabel för att hålla en referens till MainWindowViewModel.
         private readonly MainWindowViewModel? mainWindowViewModel;
-
         private DispatcherTimer timer;
         private string _testData;
-
         private string _question;
+        private string _answerOne;
+        private string _answerTwo;
+        private string _answerThree;
+        private string _answerFour;
 
         private QuestionPackViewModel? _activePack; // Det valda frågepaketet
 
@@ -33,6 +35,44 @@ namespace Labb3_Quiz_Configurator.ViewModel
 
             set { 
                 _question = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string AnswerOne
+        {
+            get => _answerOne; 
+            set {
+                _answerOne = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string AnswerTwo
+        {
+            get => _answerTwo; 
+            set { 
+                _answerTwo = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string AnswerThree
+        {
+            get => _answerThree;
+            set
+            {
+                _answerThree = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string AnswerFour
+        {
+            get => _answerFour;
+            set
+            {
+                _answerFour = value;
                 RaisePropertyChanged();
             }
         }
@@ -65,7 +105,20 @@ namespace Labb3_Quiz_Configurator.ViewModel
             if (pack != null && pack.Questions.Any()) // Kontrollera om det finns frågor i paketet
             {
                 _activePack = pack;
-                Question = _activePack.Questions.First().Query; // Visa den första frågan
+                var firstQuestion = _activePack.Questions.First();
+
+                var answers = new List<string> { firstQuestion.CorrectAnswer };
+                answers.AddRange(firstQuestion.IncorrectAnswers.Take(3));
+
+                var random = new Random();
+                var shuffledAnswers = answers.OrderBy(a => random.Next()).ToList();
+
+
+                Question = firstQuestion.Query;
+                AnswerOne = shuffledAnswers[0];
+                AnswerTwo = shuffledAnswers[1];
+                AnswerThree = shuffledAnswers[2];
+                AnswerFour = shuffledAnswers[3];
             }
         }
 
