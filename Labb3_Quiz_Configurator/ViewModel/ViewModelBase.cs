a170d1252e204fd046298cade947848084bd63ed
SofiaNords <sofia.k.nordstrom.com>
2024-11-19 17:28:59 +0100

    Remove comments from ViewModelBase

--- Labb3_Quiz_Configurator/ViewModel/ViewModelBase.cs
+++ Labb3_Quiz_Configurator/ViewModel/ViewModelBase.cs
@@ -3,16 +3,12 @@ using System.Runtime.CompilerServices;
 
 namespace Labb3_Quiz_Configurator.ViewModel
 {
-    // Bas-ViewModel-klass som implementerar INotifyPropertyChanged
     public class ViewModelBase : INotifyPropertyChanged
     {
-        // Event som används för att signalera att en egenskap har ändrats.
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        // Metod för att höja (raise) PropertyChanged-eventet.
         public void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
         {
-            // Anropar eventet om det finns några lyssnare.
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
