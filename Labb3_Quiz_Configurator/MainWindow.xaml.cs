c0ec407d1a0253d4886829056395f1e7aa3591a1
SofiaNords <sofia.k.nordstrom.com>
2024-11-19 17:35:13 +0100

    Remove comments from MainWindow

--- Labb3_Quiz_Configurator/MainWindow.xaml.cs
+++ Labb3_Quiz_Configurator/MainWindow.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace Labb3_Quiz_Configurator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
