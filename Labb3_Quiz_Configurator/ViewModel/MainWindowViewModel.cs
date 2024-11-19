67881e11534b14a65a824d4e76602345e4e30e66
SofiaNords <sofia.k.nordstrom.com>
2024-11-19 13:43:44 +0100

    Remove comments from MainWindowViewModel

--- Labb3_Quiz_Configurator/ViewModel/MainWindowViewModel.cs
+++ Labb3_Quiz_Configurator/ViewModel/MainWindowViewModel.cs
@@ -3,37 +3,33 @@ using Labb3_Quiz_Configurator.Dialogs;
 using Labb3_Quiz_Configurator.Model;
 using Labb3_Quiz_Configurator.Views;
 using System.Collections.ObjectModel;
-using System.Reflection.Metadata;
 using System.Windows;
 
 namespace Labb3_Quiz_Configurator.ViewModel
 {
-    // Huvud-ViewModel för applikationens huvudfönster.
     public class MainWindowViewModel : ViewModelBase
     {
-        // Privata fält
         private object _currentView;
         private bool _isFullScreen;
 
-        // Egenskaper
-        public object CurrentView // Egenskap för att hålla aktuell vy (används för vyväxling).
+        public object CurrentView 
         {
             get => _currentView;
             set
             {
                 _currentView = value;
                 RaisePropertyChanged();
-                RaisePropertyChanged(nameof(IsEditEnabled)); // Uppdatera Edit-knappen
-                RaisePropertyChanged(nameof(IsPlayEnabled)); // Uppdatera Play-knappen
+                RaisePropertyChanged(nameof(IsEditEnabled)); 
+                RaisePropertyChanged(nameof(IsPlayEnabled)); 
             }
         }
         public ConfigurationViewModel ConfigurationViewModel { get; }
         public PlayerViewModel PlayerViewModel { get; }
-        public ObservableCollection<QuestionPackViewModel> Packs { get; set; } // ObservableCollection som håller en lista av frågepaket.
-        public bool IsEditEnabled => CurrentView is not ConfigurationView; // Egenskap för att kontrollera om Edit-knappen ska vara aktiv.
-        public bool IsPlayEnabled => CurrentView is not PlayerView; // Egenskap för att kontroller om Play-knappen ska vara aktiv.
+        public ObservableCollection<QuestionPackViewModel> Packs { get; set; } 
+        public bool IsEditEnabled => CurrentView is not ConfigurationView; 
+        public bool IsPlayEnabled => CurrentView is not PlayerView; 
        
-        // Kommandon för olika åtgärder.
+
         public DelegateCommand NewPackCommand { get; }
         public DelegateCommand OpenPackCommand { get; }
         public DelegateCommand SelectQuestionPackCommand { get; }
@@ -41,24 +37,18 @@ namespace Labb3_Quiz_Configurator.ViewModel
         public DelegateCommand SwitchToConfigurationViewCommand { get; }
         public DelegateCommand FullScreenCommand { get; }
 
-        // Konstruktorn för MainWindowViewModel.
         public MainWindowViewModel()
         {
-            // Initiera vyer och kommandon
             SwitchToPlayerViewCommand = new DelegateCommand(SwitchToPlayerView);
             SwitchToConfigurationViewCommand = new DelegateCommand(SwitchToConfigurationView);
 
-            // Initiera Packs som en tom ObservableCollection.
             Packs = new ObservableCollection<QuestionPackViewModel>();
 
-            // Initiera ViewModels
             ConfigurationViewModel = new ConfigurationViewModel(this);
             PlayerViewModel = new PlayerViewModel(this);
 
-            // Sätt initial vy
             CurrentView = new ConfigurationView();
 
-            // Kommandon för dialoger
             NewPackCommand = new DelegateCommand(OpenCreateNewPackDialog);
             
             OpenPackCommand = new DelegateCommand(OpenPackOption);
@@ -87,8 +77,8 @@ namespace Labb3_Quiz_Configurator.ViewModel
             {
                 window.WindowStyle = WindowStyle.SingleBorderWindow;
                 window.WindowState = WindowState.Normal;
-                window.Width = 800; // Sätt tillbaka till din önskade storlek
-                window.Height = 450; // Sätt tillbaka till din önskade storlek
+                window.Width = 800; 
+                window.Height = 450; 
             }
         }
 
@@ -96,31 +86,28 @@ namespace Labb3_Quiz_Configurator.ViewModel
         {
             if (selectedPack is QuestionPackViewModel pack)
             {
-                ConfigurationViewModel.ActivePack = pack; // Sätt det aktiva paketet i ConfigurationViewModel
+                ConfigurationViewModel.ActivePack = pack; 
             }
-            CurrentView = new ConfigurationView(); // Lägg till denna rad
+            CurrentView = new ConfigurationView(); 
         }
 
-        // Metoder för att växla vyer
         private void SwitchToPlayerView(object obj)
         {
-            PlayerViewModel.SetActivePack(ConfigurationViewModel.ActivePack); // Skicka det aktuella frågepaketet till PlayerViewModel
-            CurrentView = new PlayerView(); // Skapa en instans av PlayerView
+            PlayerViewModel.SetActivePack(ConfigurationViewModel.ActivePack); 
+            CurrentView = new PlayerView(); 
         }
 
         private void SwitchToConfigurationView(object obj)
         {
-            CurrentView = new ConfigurationView(); // Skapa en instans av ConfigurationView
+            CurrentView = new ConfigurationView(); 
         }
 
-        // Öppna dialog för frågepaketsalternativ
         private void OpenPackOption(object obj)
         {
             var dialog = new PackOptionsDialog(ConfigurationViewModel);
             dialog.ShowDialog();
         }
 
-        // Öppna dialog för att skapa ett nytt frågepaket
         private void OpenCreateNewPackDialog(object obj)
         {
             ConfigurationViewModel.NewPack = new QuestionPackViewModel(new QuestionPack("<Packname>"));
